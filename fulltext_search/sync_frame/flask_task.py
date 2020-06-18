4ff8e5c672a911cd4fdf9b3fc861ab5b05feda69
apo5tol <alexkuzhel.biz@gmail.com>
2020-06-18 15:21:44 +0300

    feat(flask_task): Добавил решение практического задания по Flask

---
+++ fulltext_search/sync_frame/flask_task.py
@@ -0,0 +1,12 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route('/client/info', methods=['GET'])
+def client_info():
+    return {
+        'content-type': 'application/json',
+        'user_agent': str(request.user_agent)
+    }
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8000)
