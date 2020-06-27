ad0ca8cbde0260569917545f342c568344cc614d
apo5tol <alexkuzhel.biz@gmail.com>
2020-06-27 21:39:43 +0300

    feat(flask_api): Добавил api получения информации о фильме

---
+++ fulltext_search/flask_api/main.py
@@ -0,0 +1,45 @@
+import json
+from flask import Flask, request
+from flask import abort
+import requests
+
+ELASTIC_URL = "http://127.0.0.1:9200"
+
+app = Flask(__name__)
+
+
+def prepare_actors_data(actors):
+    result = []
+    for actor in actors:
+        result.append({"id": int(actor["id"]), "name": actor["name"]})
+
+    return result
+
+
+@app.route("/movies/<movie_id>", methods=["GET"])
+def movie_info(movie_id):
+    movie_info_url = "{url}/movies/_doc/{movie_id}".format(
+        url=ELASTIC_URL, movie_id=movie_id
+    )
+    response = requests.get(movie_info_url)
+    resp_data = response.json()
+    if not resp_data["found"]:
+        return abort(404, description="Фильм не найден")
+
+    elastic_movie_data = resp_data["_source"]
+    answer = {
+        "id": elastic_movie_data["id"],
+        "title": elastic_movie_data["title"],
+        "description": elastic_movie_data["description"],
+        "imdb_rating": elastic_movie_data["imdb_rating"],
+        "writers": elastic_movie_data["writers"],
+        "actors": prepare_actors_data(elastic_movie_data["actors"]),
+        "genre": elastic_movie_data["genre"],
+        "director": elastic_movie_data["director"],
+    }
+    return answer
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8000, debug=True)
+
